cda4727a0bd249fdb67b4ad4fc96c64e3175268c
michaelzh17 <macalzhang@gmail.com>
2018-11-12 20:35:44 -0500

    data visualization test

---
+++ data_vis/rw_visual.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# Make a random walk, and plot the points.
+rw = RandomWalk()
+rw.fill_walk()
+
+plt.scatter(rw.x_values, rw.y_values, s=15)
+plt.show()
