193957ad621ccf17b9ca77a9db9a2bbdea8b96ed
michaelzh17 <macalzhang@gmail.com>
2018-11-12 21:07:24 -0500

    working with api

---
+++ api/python_repos.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import requests
+
+# Make an API call and store the response.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r=requests.get(url)
+
+print("Status Code:", r.status_code)
+
+# Store API response in a variable.
+response_dict = r.json()
+
+# Process results.
+print(response_dict.keys())
